04c8341a82d03880a88ad3548e832e00490c7096
Joshua <joshuayoo725@gmail.com>
2026-05-06 03:57:04 -0700

    config.py

---
+++ source/backend/config.py
@@ -0,0 +1,9 @@
+import os
+import json
+
+class Config:
+    #Flask session security
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev")
+
+    #Firebase credentials
+    FIREBASE_CREDENTIALS = os.getenv("FIREBASE_CREDENTIALS")
